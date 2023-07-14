4ab53c3cf75fade537cc42c00875a604f4d19f2c
Jessica B <129120706+JessicaRaven@users.noreply.github.com>
2023-07-14 15:51:15 -0400

    add reset button

--- EtchASketch.js
+++ EtchASketch.js
@@ -5,31 +5,42 @@ let row=[];
 let boxes=[];
 let sideLength=16;
 let boxIndex=0;
-for(let i=0;i<=(sideLength-1);i++){
-    row[i]=document.createElement('div');
-    canvas.appendChild(row[i]);
-    row[i].classList.add("row");
+//reset buttons
+const reset= document.querySelector('.reset');
+const resolution =document.querySelector('.resolution');
 
-    for(let j=0;j<=(sideLength-1);j++){
-        boxes[boxIndex]=document.createElement('div');
-        row[i].appendChild(boxes[boxIndex]);
-        boxes[boxIndex].classList.add("box");
-        boxIndex++;
+reset.addEventListener("click",eraseCanvas);
+
+function generateCanvas(){
+    for(let i=0;i<=(sideLength-1);i++){
+        row[i]=document.createElement('div');
+        canvas.appendChild(row[i]);
+        row[i].classList.add("row");
+
+        for(let j=0;j<=(sideLength-1);j++){
+            boxes[boxIndex]=document.createElement('div');
+            row[i].appendChild(boxes[boxIndex]);
+            boxes[boxIndex].classList.add("box");
+            boxIndex++;
+        }
     }
 }
+generateCanvas();
 
 for (const box of boxes){
     box.addEventListener("mouseover", draw);
-    console.log("box");
 }
 
 function draw(e){
-    console.log("test");
     pixel=e.target;
     pixel.classList.add("boxHover");
-    console.log(pixel);
 
 
 }
 
-
+function eraseCanvas(){
+    for(const pixel of boxes){
+        console.log("test");
+        pixel.classList.remove("boxHover");
+    }
+}
