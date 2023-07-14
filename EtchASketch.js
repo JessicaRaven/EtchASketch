1f5888aea874252321a1060f406ca4e7bfd5c874
Jessica B <129120706+JessicaRaven@users.noreply.github.com>
2023-07-14 15:27:25 -0400

    add drawing function

--- EtchASketch.js
+++ EtchASketch.js
@@ -1,19 +1,35 @@
 const canvas = document.querySelector("#Canvas");
 
-//generate canvas 16x16
+//generate canvas 16x16 or custom side length later
 let row=[];
-let box=[];
+let boxes=[];
 let sideLength=16;
+let boxIndex=0;
 for(let i=0;i<=(sideLength-1);i++){
     row[i]=document.createElement('div');
     canvas.appendChild(row[i]);
     row[i].classList.add("row");
 
     for(let j=0;j<=(sideLength-1);j++){
-        box[j]=document.createElement('div');
-        row[i].appendChild(box[j]);
-        box[j].classList.add("box");
+        boxes[boxIndex]=document.createElement('div');
+        row[i].appendChild(boxes[boxIndex]);
+        boxes[boxIndex].classList.add("box");
+        boxIndex++;
     }
+}
+
+for (const box of boxes){
+    box.addEventListener("mouseover", draw);
+    console.log("box");
+}
+
+function draw(e){
+    console.log("test");
+    pixel=e.target;
+    pixel.classList.add("boxHover");
+    console.log(pixel);
+
+
 }
 
 
